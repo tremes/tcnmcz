20ed00ffdb842082449d23d4963d196b783e0aee
Tomáš Remeš <tremes@redhat.com>
2017-10-08 18:13:08 +0200

    Update - use background img in the header

--- js/logo-toggle.js
+++ js/logo-toggle.js
@@ -6,10 +6,12 @@ $(images[2]).css("display", "block");
 
 function toggleImgVisibility() {
 
+	$(images[0]).toggle();
 	$(images[1]).toggle();
 	$(images[2]).toggle();
 	$(images[3]).toggle();
 	$(images[4]).toggle();
+	$(images[5]).toggle();
 }
 
 
