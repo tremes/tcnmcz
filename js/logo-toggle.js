4cbb455f625d01187590167db2995c4ecdd531c4
Tomáš Remeš <tremes@redhat.com>
2018-12-16 13:27:38 +0100

    Remove manager name & update JS files

--- js/logo-toggle.js
+++ js/logo-toggle.js
@@ -8,25 +8,9 @@ $(images[1]).css("display", "inline");
 $(images[2]).css("display", "inline");
 
 function toggleImgVisibility() {
-
-	$(images[0]).toggle();
-	$(images[1]).toggle();
-	$(images[2]).toggle();
-	$(images[3]).toggle();
-	$(images[4]).toggle();
-	$(images[5]).toggle();
+	$(images).each(function(){
+		$(this).toggle();
+	});
 }
 
-
 setInterval(toggleImgVisibility, 4000);
-
-// INITIALIZE BLUEIMP GALLERY
-
-document.getElementById('links').onclick = function (event) {
-	event = event || window.event;
-	var target = event.target || event.srcElement,
-		link = target.src ? target.parentNode : target,
-		options = {index: link, event: event},
-		links = this.getElementsByTagName('a');
-	blueimp.Gallery(links, options);
-};
